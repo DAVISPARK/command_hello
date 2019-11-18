0422da0dac788efb75df1ed8f860bbe5c81f31f8
DAVIS PARK <zinuriu2@gmail.com>
2019-11-18 10:40:37 +0900

    class 제거

--- hello.java
+++ hello.java
@@ -1,8 +1,7 @@
-public class hello {
 public static void main(String[] args) {
 	system.out.print("hello");
 	system.out.print("bye");
 	system.out.print("good");
 	system.out.print("fuck");
 	}
-}
+
